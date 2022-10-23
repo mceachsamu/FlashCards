077968a13c4e3a7d9141d23497944d698750408b
mceachsamu <samisacamel@outlook.com>
2022-10-24 11:30:20 +1300

    initial commit

---
+++ Flash Cards/FlashCard.swift
@@ -0,0 +1,8 @@
+//
+//  FlashCard.swift
+//  Flash Cards
+//
+//  Created by sam mceachern on 23/10/22.
+//
+
+import Foundation
---
+++ Flash Cards/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Flash Cards
+//
+//  Created by sam mceachern on 23/10/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
